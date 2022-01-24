e00fe2a05b8bc2f39e7564d5251f4e33c9e852a6
LineBreez <alinesn0308@gmail.com>
2022-01-23 22:10:30 -0300

    desafio03

---
+++ script.js
@@ -0,0 +1,12 @@
+for(let i = 1; i <= 10; i++) {
+    console.log(i);
+}
+int i = 10;
+while (i <= 1)
+{
+for(let i = 1; i <= 100; i += 2){
+    console.log(i);
+}
+ for(let i = 0; i <= 100; i += 2){
+    console.log(i);
+}
